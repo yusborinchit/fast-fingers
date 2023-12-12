80c9225d5385b0b942e8f9d9ee9146abe46646f2
Santiago Garcia <just.b000ringsh1t@gmail.com>
2023-12-12 19:56:52 -0300

    feat: :tada: vite project with react and tailwindcss setup

---
+++ src/app.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="max-w-screen-md px-4 mx-auto">
+      <h1 className="py-2 text-2xl font-bold tracking-tighter">Hello World</h1>
+    </main>
+  );
+}
+
+export default App;
